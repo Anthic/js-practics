e4c2779f73798e70af3039fca6d049028e3644f7
Anthic Kumar Singh <anthickumarsingh2@gmail.com>
2026-03-26 11:48:56 +0600

    done

--- pattern programming/problem2.js
+++ pattern programming/problem2.js
@@ -1,4 +1,4 @@
-//solve this pattern 
+//solve this pattern
 /**
 A
 A B
@@ -8,14 +8,13 @@ A B C D
  */
 
 let prompt = require("prompt-sync")();
-let n = Number(prompt("Enter your printable number"))
+let n = Number(prompt("Enter your printable numbers again.... and save it"));
 
 for (let i = 1; i <= n; i++) {
-    let ascii = 65
-    for (let j = 1; j <= i; j++) {
-       process.stdout.write(String.fromCharCode(ascii)+ "")
-        ascii++
-    }
-    console.log()
+  let ascii = 65;
+  for (let j = 1; j <= i; j++) {
+    process.stdout.write(String.fromCharCode(ascii) + "");
+    ascii++;
+  }
+  console.log();
 }
-
